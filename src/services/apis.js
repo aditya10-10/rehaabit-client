3996e3c96e85a70a4cceb81c1b5aa4edb7296360
aditya10_10 <ascs25835@gmail.com>
2024-11-01 08:34:31 +0530

    env

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,6 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL =
+  process.env.REACT_APP_BASE_URL ||
+  "https://rehaabit-server-production.up.railway.app/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
